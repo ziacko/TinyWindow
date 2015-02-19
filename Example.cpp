f72d62c8c73fa73340c1c138d97b0da751fc637b
Ziyad Barakat <ziyadbarakat@gmail.com>
2015-02-19 16:53:42 +1100

    fixed GLX bug on the Linux platform that would immediately crash the program. This was strangley caused by the fact that when calling Linux_InitOpenGL to return a value, the function itself would be skipped over. I do not know why this occurs bust I managed a quick fix by instead simply calling the function and having the calling function return GL_TRUE. I need to look at the cause of this problem.

--- Example.cpp
+++ Example.cpp
@@ -15,13 +15,13 @@ int main()
 
 	WindowManager::SetWindowOnKeyEvent("Example", blarg);
 
+	printf("Blarg\n");
 	while (!WindowManager::GetWindowShouldClose("Example"))
-	{ 	
+	{	
 			glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 			WindowManager::PollForEvents();
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			WindowManager::MakeWindowCurrentContext("Example");
-
 			WindowManager::WindowSwapBuffers("Example");
 	}
 
