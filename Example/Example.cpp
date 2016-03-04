9da87eb63aeef58d66198e6acef83ef2fed82d4c
ziyad barakat <ziyadbarakat@gmail.com>
2016-03-04 19:12:39 +1100

    fixed issue with PrintErrorMessage being accidentally called on shutdown. also swapped out noexcept with throw() as noexcept is not supported by older versions of visual studio

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -19,10 +19,10 @@ int main()
 	{
 		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		windowManager::PollForEvents();// or WaitForEvents
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 		windowManager::MakeWindowCurrentContextByIndex(0);
 		windowManager::WindowSwapBuffersByIndex(0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	windowManager::ShutDown();
