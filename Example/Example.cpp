088d5ee65b5b59563921ae73235754cffe9662a6
ziyad barakat <ziyadbarakat@gmail.com>
2016-03-13 01:12:37 +1100

    added typedefs for function callbacks and implemented std::is_error_code_enum to automatically convert error_t enums to std::error_code

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -22,7 +22,7 @@ int main()
 		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		manager->PollForEvents();// or WaitForEvents
 		
-		manager->MakeWindowCurrentContext(window);
+		//manager->MakeWindowCurrentContext(window);
 		manager->SwapWindowBuffers(window);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
