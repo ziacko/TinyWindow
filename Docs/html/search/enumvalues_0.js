83d2657ac20e6daeba7c36eaec0f949d5f051d59
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-03-24 23:25:38 -0700

    Removed a bunch of files. Added instructions
    Former-commit-id: 2ad5ec7c7ad0105e35c571950149b8dbc9c76337 [formerly fb6a49b64f6e9d3f089f437120ba89f26c1e67c4]
    Former-commit-id: 6b9a32d8988f7e058d3806a9b98904584f266e69
    Former-commit-id: 6b9d9dc79a111e2b981ae1ef5692460f97c7246f

--- Docs/html/search/enumvalues_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['alreadyinitialized',['alreadyInitialized',['../d7/dc6/namespace_tiny_window_abc54dc4e5c2d3641b743ddef22b31d61.html#abc54dc4e5c2d3641b743ddef22b31d61ab51d6dab282a90a6eed7e95e5766881b',1,'TinyWindow']]],
-  ['arrowdown',['arrowDown',['../d7/dc6/namespace_tiny_window_a67d27ff1c8b00549a252112c52a1f7ca.html#a67d27ff1c8b00549a252112c52a1f7caac2c7927a0dc8284d73801c83cff1f42d',1,'TinyWindow']]],
-  ['arrowleft',['arrowLeft',['../d7/dc6/namespace_tiny_window_a67d27ff1c8b00549a252112c52a1f7ca.html#a67d27ff1c8b00549a252112c52a1f7caa1dc9682aa36d64717fcdf13fe8bcf75a',1,'TinyWindow']]],
-  ['arrowright',['arrowRight',['../d7/dc6/namespace_tiny_window_a67d27ff1c8b00549a252112c52a1f7ca.html#a67d27ff1c8b00549a252112c52a1f7caac559b1508367052c0c8a925aa1b00a92',1,'TinyWindow']]],
-  ['arrowup',['arrowUp',['../d7/dc6/namespace_tiny_window_a67d27ff1c8b00549a252112c52a1f7ca.html#a67d27ff1c8b00549a252112c52a1f7caa39e2be3b89723d2662743f92bc691672',1,'TinyWindow']]]
-];
--- Docs/html/search/variables_d.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['width',['width',['../d2/d6a/struct_tiny_window_1_1ui_vec2_a34dd165ab7aa5e36ed3debb38876d1d3.html#a34dd165ab7aa5e36ed3debb38876d1d3',1,'TinyWindow::uiVec2']]],
-  ['windowhandle',['windowHandle',['../d0/d80/struct_tiny_window_1_1window__t_adabcd829ed30efb8907251f295a30b38.html#adabcd829ed30efb8907251f295a30b38',1,'TinyWindow::window_t']]],
-  ['windowlist',['windowList',['../d8/d4d/class_tiny_window_1_1window_manager_a5c44f4ad59f5d18446eafd7c1cb71924.html#a5c44f4ad59f5d18446eafd7c1cb71924',1,'TinyWindow::windowManager']]]
-];
