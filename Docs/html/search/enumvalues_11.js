2fc4c93279b8d69efb76dbd4c2d6edda7a38c61a
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-03-24 23:25:38 -0700

    Removed a bunch of files. Added instructions
    Former-commit-id: 2ad5ec7c7ad0105e35c571950149b8dbc9c76337 [formerly 2665e682245a00c6cbbaebb09ab47f0bd72b96c1 [formerly fb6a49b64f6e9d3f089f437120ba89f26c1e67c4]]
    Former-commit-id: 6b9a32d8988f7e058d3806a9b98904584f266e69
    Former-commit-id: 6b9d9dc79a111e2b981ae1ef5692460f97c7246f
    Former-commit-id: 83d2657ac20e6daeba7c36eaec0f949d5f051d59

--- Docs/html/search/enumvalues_11.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['windowinvalid',['windowInvalid',['../d7/dc6/namespace_tiny_window_abc54dc4e5c2d3641b743ddef22b31d61.html#abc54dc4e5c2d3641b743ddef22b31d61a25879b3d04508890f2cfdd544dfb019e',1,'TinyWindow']]],
-  ['windowscannotcreatewindows',['windowsCannotCreateWindows',['../d7/dc6/namespace_tiny_window_abc54dc4e5c2d3641b743ddef22b31d61.html#abc54dc4e5c2d3641b743ddef22b31d61a702b82198565187af163c913294984e7',1,'TinyWindow']]],
-  ['windowscannotinitialize',['windowsCannotInitialize',['../d7/dc6/namespace_tiny_window_abc54dc4e5c2d3641b743ddef22b31d61.html#abc54dc4e5c2d3641b743ddef22b31d61a1b61d838931367e4f3ed105602d272aa',1,'TinyWindow']]],
-  ['windowsfunctionnotimplemented',['windowsFunctionNotImplemented',['../d7/dc6/namespace_tiny_window_abc54dc4e5c2d3641b743ddef22b31d61.html#abc54dc4e5c2d3641b743ddef22b31d61a696fde84bffaf263b3ba0c00aa673cc5',1,'TinyWindow']]]
-];
--- Docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tinywindow',['TinyWindow',['../md_C:_Users_ziyad_Documents_Portfolio_dependencies_tinywindow_README.html',1,'']]]
-];
